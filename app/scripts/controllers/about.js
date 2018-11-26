76351a6ffdbbf1b893ccc0ea3355464e347ff4b9
phaseout <mmomain@gmail.com>
2018-11-26 10:55:27 -0500

    Added AngularJS Boilerplate -- Boilerplate generated automatically not written by me

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name decidrApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the decidrApp
+ */
+angular.module('decidrApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name decidrApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the decidrApp
+ */
+angular.module('decidrApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
