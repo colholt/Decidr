e87b8cce60cd44a0369b3aec4bfe4db4eec5201f
phaseout <mmomain@gmail.com>
2018-12-03 22:49:12 -0500

    bunch of frontend changes

--- app/Backend/main.py
+++ app/Backend/main.py
@@ -20,22 +20,24 @@ mysql.init_app(app)
 conn = mysql.connect()
 cursor = conn.cursor()
 
+
 def event_stream(roomID):
     pubsub = red.pubsub()
     pubsub.subscribe('users')
     pubsub.subscribe('choices')
     pubsub.subscribe('decisions')
     for new_user in pubsub.listen():
-        try:        
+        try:
             vals = ast.literal_eval(new_user['data'])
             print 'vals:'
             print vals['roomID']
-            print 'rid:',roomID
+            print 'rid:', roomID
             if str(roomID) == str(vals['roomID']):
-                yield "data: %s\n\n" % new_user
+                yield ("data: %s\n\n" % new_user).decode('utf-8')
         except (KeyError, ValueError) as e:
-            print 'err:',e
-            yield "data: %s\n\n" % new_user
+            print 'err:', e
+            yield ("data: %s\n\n" % new_user).decode('utf-8')
+
 
 @app.route('/')
 def hello():
@@ -48,10 +50,12 @@ def submit():
     conn.commit()
     return jsonify({"insertID": cursor.lastrowid}), 200
 
+
 @app.route('/subscribe')
 def sub_scribe():
     rid = request.args.get('roomid')
     return Response(event_stream(rid), mimetype="text/event-stream")
+
 
 @app.route('/makeDecision', methods=['POST'])
 def make_decision():
@@ -62,9 +66,12 @@ def make_decision():
     userID = data['userID']
     choiceID = data['choiceID']
     decision = data['decision']
-    cursor.execute('INSERT INTO decisions VALUES(null, %s, %s, %s, %s)', (choiceID, userID, roomID, decision))
-    red.publish('decisions', u'{"type": "decision", "roomID": %s, "choiceID": %s, "decision": %s}' % (roomID, choiceID, decision))
+    cursor.execute('INSERT INTO decisions VALUES(null, %s, %s, %s, %s)',
+                   (choiceID, userID, roomID, decision))
+    red.publish('decisions', u'{"type": "decision", "roomID": %s, "choiceID": %s, "decision": %s}' % (
+        roomID, choiceID, decision))
     return '', 200
+
 
 '''
 Body Parameters
@@ -73,6 +80,8 @@ Body Parameters
 Returns
         good vibes
 '''
+
+
 @app.route('/addChoice', methods=['POST'])
 def add_choice():
     data = request.get_json()
@@ -80,12 +89,15 @@ def add_choice():
         return '', 500
     roomID = data['roomID']
     choice = data['choice']
-    cursor.execute("INSERT INTO choices VALUES(null, %s, %s)", (choice, str(roomID)))
+    cursor.execute("INSERT INTO choices VALUES(null, %s, %s)",
+                   (choice, str(roomID)))
     choiceID = cursor.lastrowid
     conn.commit()
-    red.publish('choices', u'{"type": "choice", "roomID": "%s", "choice": "%s", "choiceID": %s}' % (str(roomID), choice, choiceID))
+    red.publish('choices', u'{"type": "choice", "roomID": "%s", "choice": "%s", "choiceID": %s}' % (
+        str(roomID), choice, choiceID))
     # red.publish('choices', u'RID:%s: CHOICE ADDED: %s' % (str(roomID), choice))
     return '', 200
+
 
 '''
 Body Parameters
@@ -96,6 +108,8 @@ Returns
         roomName
         userID
 '''
+
+
 @app.route('/joinRoom', methods=['POST'])
 def join_room():
     data = request.get_json()
@@ -104,17 +118,24 @@ def join_room():
     roomID = data['roomID']
     cursor.execute("SELECT * FROM rooms WHERE id=%s", roomID)
     room = cursor.fetchall()
-
+    print 'room: ', room
     if len(room) != 0:
-        cursor.execute("INSERT INTO users VALUES(null, %s, %s)", (data['name'], str(room[0][0])))
+        cursor.execute("INSERT INTO users VALUES(null, %s, %s)",
+                       (data['name'], str(room[0][0])))
         userID = cursor.lastrowid
         conn.commit()
-        red.publish('users', u'{"type": "", "roomID": "%s", "name": "%s"}' % (str(roomID), data['name']))
+        red.publish('users', u'{"type": "", "roomID": "%s", "name": "%s"}' % (
+            str(roomID), data['name']))
         # red.publish('users', u'RID:%s: USER JOINED: %s' % (str(roomID), data['name']))
-        return jsonify({"roomID": room[0][0], "roomName": room[0][1], "userID": userID}), 200
+        cursor.execute("SELECT * FROM choices WHERE rid=%s", roomID)
+        choices = []
+        for i in cursor.fetchall():
+            choices.append(list(i))
+        return jsonify({"roomID": room[0][0], "roomName": room[0][1], "userID": userID, "choices": choices}), 200
     else:
         return 'RoomID not valid', 500
     return '', 200
+
 
 '''
 Body Parameters
@@ -124,16 +145,20 @@ Returns
         userID
         roomID
 '''
+
+
 @app.route('/createRoom', methods=['POST'])
 def create_room():
-    data = request.get_json() # get json data from post request
+    data = request.get_json()  # get json data from post request
     if data == None:
         return '', 500
     cursor.execute("INSERT INTO rooms VALUES(null, %s)", data['roomName'])
     roomID = cursor.lastrowid
-    cursor.execute("INSERT INTO users VALUES(null, %s, %s)", (data['name'], str(roomID)))
+    cursor.execute("INSERT INTO users VALUES(null, %s, %s)",
+                   (data['name'], str(roomID)))
     conn.commit()
     return jsonify({"userID": cursor.lastrowid, "roomID": roomID}), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,51 @@
  * Controller of the decidrApp
  */
 angular.module('decidrApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.changeView = function(name) {
+  .controller('MainCtrl', function ($scope, $http) {
+    var roomID;
+    var userID;
+    $scope.idError = false;
+
+    $scope.joinRoom = function () {
+      var data = { "name": $scope.name, "roomID": $scope.rid };
+      $http.post('http://68.183.140.180:5000/joinRoom', data).then(function (res) {
+        // handle successful room join
+        roomID = res.data.roomID;
+        userID = res.data.userID;
+        $scope.idError = false;
+        var url = "http://68.183.140.180:5000/subscribe?roomid=";
+        url += roomID;
+        $scope.roomName = res.data.roomName;
+        $scope.choices = res.data.choices;
+        $scope.changeView("view");
+        var source = new EventSource(url); //setup event source for SSE
+        
+        source.onmessage = function (event) {
+          // process event from server
+          var data = event.data.split('\n');
+          console.log(data);
+          var fixed_data = JSON.parse(data[0].split("'data': ")[1].split("'}")[0].slice(1));
+          if (fixed_data.type === "choice") {
+            $scope.choices.push([fixed_data.choiceID, fixed_data.choice]);
+            console.log($scope.choices);
+          }
+        };
+
+        console.log("userid,roomid:", [userID, roomID]);
+        console.log("success:", res);
+      }, function (res) {
+        // handle room not joined
+        if (res.data === "RoomID not valid") {
+          $scope.idError = true;
+        }
+
+        console.log("err:", res);
+      });
+    };
+    
+
+    // Function to change view
+    $scope.changeView = function (name) {
       if (name === "join") {
         $scope.joining = true;
         $scope.creating = false;
@@ -18,9 +61,15 @@ angular.module('decidrApp')
         $scope.joining = false;
         $scope.creating = true;
       }
+      if (name === "view") {
+        $scope.joining = false;
+        $scope.creating = false;
+        $scope.view = true;
+      }
       if (name === "home") {
         $scope.joining = false;
         $scope.creating = false;
+        $scope.view = false;
       }
     };
   });
