90a585455625737ecdb4beb5846e649ad3d75004
phaseout <mmomain@gmail.com>
2018-12-03 23:10:44 -0500

    added create room function for frontend

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,6 +13,34 @@ angular.module('decidrApp')
     var userID;
     $scope.idError = false;
 
+    $scope.createRoom = function () {
+      var data = { "name": $scope.name, "roomName": $scope.roomName };
+      $http.post('http://68.183.140.180:5000/createRoom', data).then(function (res) {
+        roomID = res.data.roomID;
+        userID = res.data.userID;
+        console.log("room id:", res.data.roomID);
+        var url = "http://68.183.140.180:5000/subscribe?roomid=";
+        url += roomID;
+        $scope.choices = [];
+        var source = new EventSource(url); //setup event source for SSE
+        source.onmessage = function (event) {
+          // process event from server
+          var data = event.data.split('\n');
+          console.log(data);
+          var fixed_data = JSON.parse(data[0].split("'data': ")[1].split("'}")[0].slice(1));
+          if (fixed_data.type === "choice") {
+            $scope.$apply(function () {
+              $scope.choices.push([fixed_data.choiceID, fixed_data.choice]);
+            });
+            console.log($scope.choices);
+          }
+        };
+        $scope.changeView("view");
+      }, function (res) {
+        console.log("failure");
+      });
+    };
+
     $scope.joinRoom = function () {
       var data = { "name": $scope.name, "roomID": $scope.rid };
       $http.post('http://68.183.140.180:5000/joinRoom', data).then(function (res) {
@@ -26,14 +54,16 @@ angular.module('decidrApp')
         $scope.choices = res.data.choices;
         $scope.changeView("view");
         var source = new EventSource(url); //setup event source for SSE
-        
+
         source.onmessage = function (event) {
           // process event from server
           var data = event.data.split('\n');
           console.log(data);
           var fixed_data = JSON.parse(data[0].split("'data': ")[1].split("'}")[0].slice(1));
           if (fixed_data.type === "choice") {
-            $scope.choices.push([fixed_data.choiceID, fixed_data.choice]);
+            $scope.$apply(function () {
+              $scope.choices.push([fixed_data.choiceID, fixed_data.choice]);
+            });
             console.log($scope.choices);
           }
         };
@@ -49,7 +79,7 @@ angular.module('decidrApp')
         console.log("err:", res);
       });
     };
-    
+
 
     // Function to change view
     $scope.changeView = function (name) {
